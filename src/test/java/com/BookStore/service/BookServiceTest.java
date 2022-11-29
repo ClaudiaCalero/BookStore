14c900cc435e609f2127b1a12e2c1d1e4fc64bea
ClaudiaCalero <claudiaclrduro@gmail.com>
2022-11-29 20:26:53 +0100

    first test

---
+++ src/test/java/com/BookStore/service/BookServiceTest.java
@@ -0,0 +1,23 @@
+package com.BookStore.service;
+
+import com.BookStore.model.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookServiceTest {
+        @Autowired
+        BookService bookService;
+
+        @Test
+        public void addBooks() {
+            Book book = new Book();
+            book.setTitle("Pride & Prejudice");
+            book.setAuthor("Jane Auten");
+            book.setGenre("Romance/Drama");
+            book.setLaunchDate("12-01-1813");
+
+            bookService.saveBook(book);
+        }
+}
